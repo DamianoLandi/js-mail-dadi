0f680826ba3d8aac355087d1ac872505ae8fc65f
DamianoLandi <damianolandi01@gmail.com>
2021-07-04 18:20:52 +0200

    Raccolta Variabili

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log("ok");
+
+//MAIL
+
+//Raccolta variabili
+
+var mail = prompt("Inserisci la tua email");
+
+var i = 0;
+
+var mailList = ["ciao@gmail.com", "biscotto@yahoo.com", "pizza@libero.com", "pasta@gmail.com"];
+
+var result = document.getElementById("result");
+
+var reason = document.getElementById("reason");
+
