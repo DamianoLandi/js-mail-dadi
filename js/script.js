972cdb18ed785d4537f9b095e39da8b7da97b33e
DamianoLandi <damianolandi01@gmail.com>
2021-07-04 19:08:13 +0200

    Verifica Risultati e Print in HTML

--- js/script.js
+++ js/script.js
@@ -36,6 +36,8 @@ else{
 
 //DADI
 
+//Raccolta Variabili
+
 var score1 = (Math.floor(Math.random() * 6) + 1);
 
 var score2 = (Math.floor(Math.random() * 6) + 1);
@@ -45,3 +47,20 @@ var winner = document.getElementById("winner");
 var playerScore = document.getElementById("playerScore");
 
 var CPUScore = document.getElementById("CPUScore");
+
+//Verifica Vincitore e Print
+
+if (score1 > score2) {
+    winner.innerHTML = "IL vincitore è il Player";
+}
+
+else if (score1 < score2) {
+    winner.innerHTML = "Il vincitore è il CPU";
+}
+
+else{
+    winner.innerHTML = "La partita si conclude in parità";
+}
+
+playerScore.innerHTML = score1;
+CPUScore.innerHTML = score2;
